91a2b519391647198a58a5bdda411f97185bd0d2
Elias Keller <elias.kelle@gmail.com>
2019-12-20 03:24:42 +0100

    feat: videoComponent with youtube-video

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import {MatIconModule, MatTabsModule} from '@angular/material';
+import { MatTabsModule} from '@angular/material';
 import { MatCardModule } from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
+import { VideoComponent } from './video/video.component';
+import {YouTubePlayerModule} from '@angular/youtube-player';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    VideoComponent
   ],
   imports: [
     BrowserModule,
@@ -20,6 +23,7 @@ import { HomeComponent } from './home/home.component';
     BrowserAnimationsModule,
     MatTabsModule,
     MatCardModule,
+    YouTubePlayerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
