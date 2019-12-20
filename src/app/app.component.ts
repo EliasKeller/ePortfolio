7caacd36d99e40e937f7358673426bd8391443e0
Elias Keller <elias.keller@abraxas.ch>
2019-12-20 16:05:55 +0100

    feat: floating button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,15 @@ import {Component, ViewEncapsulation} from '@angular/core';
 export class AppComponent {
   private title = 'ePortfolio';
   private logo = 'assets/images/Logo.svg';
+
+  private goToTop(): void {
+    const scrollToTop = window.setInterval(() => {
+      const pos = window.pageYOffset;
+      if (pos > 0) {
+        window.scrollTo(0, pos - 20);
+      } else {
+        window.clearInterval(scrollToTop);
+      }
+    }, 16);
+  }
 }
